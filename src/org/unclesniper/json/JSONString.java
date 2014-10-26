951e1e0384664171a67505084cb7dfc77368291d
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2014-10-26 02:30:57 +0200

    added JSONString.escapeString()

--- src/org/unclesniper/json/JSONString.java
+++ src/org/unclesniper/json/JSONString.java
@@ -2,6 +2,8 @@ package org.unclesniper.json;
 
 public class JSONString extends JSONPrimitive {
 
+	private static final char[] hexChars = "0123456789ABCDEF".toCharArray();
+
 	private String value;
 
 	public JSONString(String value) {
@@ -20,6 +22,50 @@ public class JSONString extends JSONPrimitive {
 
 	public int getJSONType() {
 		return JSON.TYPE_STRING;
+	}
+
+	public static String escapeString(String value, boolean quote) {
+		StringBuilder builder = new StringBuilder();
+		if(quote)
+			builder.append('"');
+		int i, length = value.length();
+		char c;
+		for(i = 0; i < length; ++i)
+			switch(c = value.charAt(i)) {
+				case '"':
+				case '\\':
+					builder.append('\\');
+					builder.append(c);
+					break;
+				case '\b':
+					builder.append("\\b");
+					break;
+				case '\f':
+					builder.append("\\f");
+					break;
+				case '\n':
+					builder.append("\\n");
+					break;
+				case '\r':
+					builder.append("\\r");
+					break;
+				case '\t':
+					builder.append("\\t");
+					break;
+				default:
+					if(c < ' ') {
+						builder.append("\\u");
+						int code = (int)c;
+						for(i = 12; i >= 0; i -= 4)
+							builder.append(JSONString.hexChars[(code >> i) & 0xF]);
+					}
+					else
+						builder.append(c);
+					break;
+			}
+		if(quote)
+			builder.append('"');
+		return builder.toString();
 	}
 
 }
